2ea162f3cb0016e0ae7811e0002dbe96b060d7c9
Nipuna Kaweya <nipuna609@gmail.com>
2024-07-11 00:00:37 +0530

    Server Updated

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 const express = require('express');
-const { createPostEndpoint, createGetEndpoint } = require('./aid_nexus'); // Adjust the path as needed
+const bcrypt = require('bcrypt');
+const { aid_nexus,createPostEndpoint, createGetEndpoint } = require('./aid_nexus'); // Adjust the path as needed
 
 const app = express();
+const router = express.Router();
 app.use(express.json());
+
+// ***************************************************Event***************************************************************
 
 // POST endpoint for adding an event
 createPostEndpoint(
     app,
     '/api/events',
     'INSERT INTO event (event_name, event_description, event_datetime, is_event_active) VALUES (?, ?, ?, ?)',
-    ['event_name', 'event_description', 'event_datetime', 'active']
+    ['event_name', 'event_description', 'event_datetime', 'active'],
+    "Event Is Successfully Registered",
 );
 
 // GET endpoint for retrieving events
@@ -26,6 +31,45 @@ createGetEndpoint(
     'SELECT * FROM event WHERE event_id = ?',
     ['id']
 );
+//*******************************************************User*******************************************************************************
+// Register New User
+/*app.post('api/register', async (req, res) => {
+    const {username, email, password, role} = req.body;
+
+    try {
+        const [existingUser] = await pool.query('SELECT * FROM user WHERE email =?',[email])
+    }
+})*/
+
+app.post('/signup', (req, res) => {
+    const { name,username, email, password } = req.body;
+    const sql = "INSERT INTO user (username, email, password) VALUES (?, ?, ?)";
+    aid_nexus.query(sql, [username, email, password], (err, result) => {
+        if (err) {
+            console.error('Error inserting data:', err);
+            return res.status(500).json({ message: "Error in node"+err });
+        }
+        return res.status(200).json({ message: "User registered successfully" });
+    });
+});
+
+
+// GET endpoint for retrieving events
+createGetEndpoint(
+    app,
+    '/api/user',
+    'SELECT * FROM user'
+);
+
+
+
+
+
+
+
+
+
+
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
