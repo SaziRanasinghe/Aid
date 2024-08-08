6192005fab1120c88457056b17a975c0204ce2b8
Nipuna Kaweya <nipuna609@gmail.com>
2024-08-08 20:25:46 +0530

    Paypal and Stripe Payment Gateway Intergrated

--- client/src/pages/funds.js
+++ client/src/pages/funds.js
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
 import Img1 from '../assets/category-images/bg1.jpg'
 import Img2 from '../assets/category-images/don2.png'
+import {useNavigate} from "react-router-dom";
+import {PayPalButtons,usePayPalScriptReducer} from "@paypal/react-paypal-js";
+import PayPalCheckout from "./PayPalCheckout";
+import Stripe from "stripe";
+import {Elements} from "@stripe/react-stripe-js";
+import {CheckoutForm,stripePromise} from "./CheckoutForm";
+
 
 const Funds = () => {
     const [card, setCard] = useState(true);
+    const [donationAmount, setDonationAmount] = useState(2);
+    const[{options,isPending},dispatch] = usePayPalScriptReducer();
+    const navigate = useNavigate();
 
     return (
         <div>
@@ -57,88 +67,32 @@ const Funds = () => {
                                 </div>
 
                                 {card ? (
-                                    <div>
-                                        <div className="space-y-4">
-                                            <div>
-                                                <label className="block text-sm font-medium mb-1" htmlFor="card-nr">
-                                                    Card Number <span className="text-red-500">*</span>
-                                                </label>
-                                                <input
-                                                    id="card-nr"
-                                                    className="text-sm text-gray-800 bg-white border rounded leading-5 py-2 px-3 border-gray-200 hover:border-gray-300 focus:border-indigo-300 shadow-sm placeholder-gray-400 focus:ring-0 w-full"
-                                                    type="text"
-                                                    placeholder="1234 1234 1234 1234"
-                                                />
-                                            </div>
-                                            <div className="flex space-x-4">
-                                                <div className="flex-1">
-                                                    <label className="block text-sm font-medium mb-1" htmlFor="card-expiry">
-                                                        Expiry Date <span className="text-red-500">*</span>
-                                                    </label>
-                                                    <input
-                                                        id="card-expiry"
-                                                        className="text-sm text-gray-800 bg-white border rounded leading-5 py-2 px-3 border-gray-200 hover:border-gray-300 focus:border-indigo-300 shadow-sm placeholder-gray-400 focus:ring-0 w-full"
-                                                        type="text"
-                                                        placeholder="MM/YY"
-                                                    />
-                                                </div>
-                                                <div className="flex-1">
-                                                    <label className="block text-sm font-medium mb-1" htmlFor="card-cvc">
-                                                        CVC <span className="text-red-500">*</span>
-                                                    </label>
-                                                    <input
-                                                        id="card-cvc"
-                                                        className="text-sm text-gray-800 bg-white border rounded leading-5 py-2 px-3 border-gray-200 hover:border-gray-300 focus:border-indigo-300 shadow-sm placeholder-gray-400 focus:ring-0 w-full"
-                                                        type="text"
-                                                        placeholder="CVC"
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div>
-                                                <label className="block text-sm font-medium mb-1" htmlFor="card-name">
-                                                    Name on Card <span className="text-red-500">*</span>
-                                                </label>
-                                                <input
-                                                    id="card-name"
-                                                    className="text-sm text-gray-800 bg-white border rounded leading-5 py-2 px-3 border-gray-200 hover:border-gray-300 focus:border-indigo-300 shadow-sm placeholder-gray-400 focus:ring-0 w-full"
-                                                    type="text"
-                                                    placeholder="John Doe"
-                                                />
-                                            </div>
-                                            <div>
-                                                <label className="block text-sm font-medium mb-1" htmlFor="card-email">
-                                                    Email <span className="text-red-500">*</span>
-                                                </label>
-                                                <input
-                                                    id="card-email"
-                                                    className="text-sm text-gray-800 bg-white border rounded leading-5 py-2 px-3 border-gray-200 hover:border-gray-300 focus:border-indigo-300 shadow-sm placeholder-gray-400 focus:ring-0 w-full"
-                                                    type="email"
-                                                    placeholder="john@company.com"
-                                                />
-                                            </div>
-                                        </div>
-                                        <div className="mt-6">
-                                            <div className="mb-4">
-                                                <button className="font-medium text-sm inline-flex items-center justify-center px-3 py-2 border border-transparent rounded leading-5 shadow-sm transition duration-150 ease-in-out w-full bg-indigo-500 hover:bg-indigo-600 text-white focus:outline-none focus-visible:ring-2">
-                                                    Pay Rs.
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <Elements stripe={ stripePromise}>
+                                        <CheckoutForm donationAmount={donationAmount}/>
+                                    </Elements>
                                 ) : (
                                     <div>
-                                        <div className="mb-4">
-                                            <button className="font-medium text-sm inline-flex items-center justify-center px-3 py-2 border border-transparent rounded leading-5 shadow-sm transition duration-150 ease-in-out w-full bg-indigo-500 hover:bg-indigo-600 text-white focus:outline-none focus-visible:ring-2">
-                                                Pay with PayPal - Rs.
-                                            </button>
+                                        <div>
+                                            <label className="block text-sm font-medium mb-1" htmlFor="card-nr">
+                                                Donating Amount <span className="text-red-500">*</span>
+                                            </label>
+                                            <input
+                                                id="card-nr"
+                                                className="text-sm text-gray-800 bg-white border rounded leading-5 py-2 px-3 border-gray-200 hover:border-gray-300 focus:border-indigo-300 shadow-sm placeholder-gray-400 focus:ring-0 w-full"
+                                                type="number"
+                                                placeholder="Amount To Donate"
+                                                value={donationAmount}
+                                                onChange={(e) => setDonationAmount(parseFloat(e.target.value))}/>
                                         </div>
+                                        <br/>
+                                            <PayPalCheckout donationAmount={donationAmount} />
                                     </div>
                                 )}
                             </div>
                         </div>
                     </div>
                     <div className="lg:w-2/4 w-full flex justify-center items-center py-4 lg:py-0">
-                        <img className="animate-bounce py-4 mr-72 px-4 w-2/3 lg:w-full" src={Img2} alt="" />
+                        <img className="animate-bounce py-4 mr-72 px-4 w-2/3 lg:w-full" src={Img2} alt=""/>
                     </div>
                 </div>
             </section>
