d759de5b1dadf1fb3a32851001df2009cb20ce9e
Nipuna Kaweya <nipuna609@gmail.com>
2024-09-29 19:45:21 +0530

    Header profile link added to profile link

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -42,12 +42,11 @@ export default function Header() {
 
         <ul className={`flex flex-col md:flex-row md:items-center md:gap-[4vw] gap-6 absolute md:static w-full md:w-auto top-16 md:top-0 left-0 bg-gray-700/80 md:bg-transparent transition-transform transform ${menuOpen ? 'translate-y-0' : '-translate-y-full md:translate-y-0'}`}>
           <li><a href="/#" className='p-4 text-gray-200 rounded md:hover:text-blue-700'>HOME</a></li>
-          <li><a href='/aboutus' className='p-4 text-gray-200 rounded md:hover:text-blue-700'>ABOUTUS</a></li>
           <li><a href='/gallery' className='p-4 text-gray-200 rounded md:hover:text-blue-700'>GALLERY</a></li>
           <li><a href='/contactus' className='p-4 text-gray-200 rounded md:hover:text-blue-700'>CONTACT</a></li>
           <li><a href='/events' className='p-4 text-gray-200 rounded md:hover:text-blue-700'>EVENTS</a></li>
 
-          {isLoggedIn && (userRole === 'donor' || userRole === 'distributor') && (
+          {isLoggedIn && (userRole === 'donor' ) && (
               <li><a href='/donate' className='p-4 text-gray-200 rounded md:hover:text-blue-700'>DONATE</a></li>
           )}
 
@@ -58,9 +57,16 @@ export default function Header() {
           {isLoggedIn && userRole === 'receiver' && (
               <li><a href='/products' className='p-4 text-gray-200 rounded md:hover:text-blue-700'>RECEIVE</a></li>
           )}
-
+          {
+            isLoggedIn? (
+                <li><a href='/user' className='p-4 text-gray-200 rounded md:hover:text-blue-700'>PROFILE</a></li>
+            ): (
+                <li><a href='/aboutus' className='p-4 text-gray-200 rounded md:hover:text-blue-700'>ABOUTUS</a></li>
+            )
+          }
           <li>
             {isLoggedIn ? (
+
                 <button
                     onClick={handleLogout}
                     className="p-4 text-white bg-red-500 hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-[red]/90 font-medium rounded-lg text-m px-8 py-1.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2"
